e00320f93b7fee565d886524d12759272f473d89
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-02-20 17:23:33 +0100

    Computed Properties

--- Properties.playground/Contents.swift
+++ Properties.playground/Contents.swift
@@ -25,3 +25,36 @@ let manager = DataManager()
 manager.data.append("Datos")
 manager.data.append("more data")
 manager.importer.filename
+
+
+
+//Computed Properties
+
+struct Point {
+    var x = 0.0, y = 0.0
+}
+
+struct Size {
+    var width = 0.0, height = 0.0
+}
+
+struct Rect {
+    var origin = Point()
+    var size = Size()
+    
+    //Esto es una 'Computed Propertie'
+    var center: Point {
+        get {
+            Point(x: origin.x + size.width/2, y: origin.y + size.height/2)
+        }
+        set {
+            origin.x = newValue.x - size.width/2
+            origin.y = newValue.y - size.height/2
+        }
+    }
+}
+
+var square = Rect(origin: Point(x: 0, y: 0), size: Size(width: 10, height: 10))
+let initialSquareCenter = square.center
+square.center = Point(x: 18, y: 4)
+print(square.center)
