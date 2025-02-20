affd057c626c48cdeb9bf9259e6c49cfff5cb8b6
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-02-20 17:11:06 +0100

    Lazy Stored Properties

--- Properties.playground/Contents.swift
+++ Properties.playground/Contents.swift
@@ -11,3 +11,17 @@ rangeOfThreeItems.firstValue = 6
 let rangeOfFourItems = FixedLengthRange(firstValue: 0, length: 4)
 
 //rangeOfFourItems.firstValue = 10
+
+class DataImporter {
+    var filename = "data.txt"
+}
+
+class DataManager {
+    lazy var importer = DataImporter()
+    var data = [String]()
+}
+
+let manager = DataManager()
+manager.data.append("Datos")
+manager.data.append("more data")
+manager.importer.filename
