cb5063be271a85e2b594dee8731322d8d98456a8
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-02-21 16:12:36 +0100

    Arrays using subscripts

--- Subscripts.playground/Contents.swift
+++ Subscripts.playground/Contents.swift
@@ -22,3 +22,33 @@ enum Planet: Int {
 
 let mars = Planet[4]
 print(mars)
+
+
+struct Matriz {
+    let rows: Int, columns: Int
+    var grid: [Double]
+    
+    init(rows: Int, columns: Int, grid: [Double]) {
+        self.rows = rows
+        self.columns = columns
+        self.grid = Array(repeating: 0.0, count: rows * columns)
+    }
+    
+    func indexIsValid(row: Int, column: Int) -> Bool {
+        return row>=0 && column >= 0 && row < rows && column < columns
+    }
+    
+    subscript(row: Int, column: Int) -> Double {
+        get{
+            assert(indexIsValid(row: row, column: column), "Index out of range")
+            return grid[(row * columns) + column]
+        }
+        set{
+            assert(indexIsValid(row: row, column: column), "Index out of range")
+        }
+    }
+}
+
+var matrix = Matriz(rows: 2, columns: 2, grid: [0.0])
+matrix[0,1] = 1.5
+matrix[1,0] = 2.5
