07ef984a1bc091c14ced0068fcb5ff0cf79cff94
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-02-21 16:29:16 +0100

    Overriding variables and methods

--- Inheritance.playground/Contents.swift
+++ Inheritance.playground/Contents.swift
@@ -35,15 +35,24 @@ tandem.hasBasket = true
 print(tandem.description)
 
 class Car : Vehicle{
+    var gear = 1
     enum KindOfCar{
-    case electric, combustion
+        case electric, combustion
     }
     
     var kindOfCar = KindOfCar.self
     var maxSpeed = 0.0
+    override var description: String {
+        return super.description + " gear \(gear)"
+    }
 }
 
 class ElectricCar : Car{
+    override var currentSpeed: Double{
+        didSet{
+            gear = Int(currentSpeed / 15.0) + 1
+        }
+    }
     var autonomy = 0.0
 }
 
@@ -51,12 +60,41 @@ class CombustionCar : Car{
     var performance = 0.0
 }
 
+let normalCar = Car()
+normalCar.currentSpeed = 60
+normalCar.gear = 4
+print(normalCar.description)
+
 let electricCar = ElectricCar()
 electricCar.kindOfCar.electric
 electricCar.maxSpeed = 220
 electricCar.autonomy = 450
+electricCar.currentSpeed = 65
+print(electricCar.description)
 
 let combustionCar = CombustionCar()
 combustionCar.kindOfCar.combustion
 combustionCar.maxSpeed = 200
 combustionCar.performance = 8
+
+
+class Train: Vehicle {
+    //'final' evita el 'override'
+    final var numberofWagons = 0
+    override func makeNoise() {
+        print("train noise")
+    }
+}
+
+let train = Train()
+train.makeNoise()
+
+tandem.makeNoise()
+
+/*
+class Railway: Train {
+    override var numberofWagons: Int {
+        
+    }
+}
+*/
