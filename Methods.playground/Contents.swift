b1b7b2b17c80747d5134589fe4c1632413b8e677
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-02-21 13:34:38 +0100

    Mutating methods

--- Methods.playground/Contents.swift
+++ Methods.playground/Contents.swift
@@ -24,8 +24,42 @@ struct Point {
         // el 'self.x' se refiere a la variable de clarada en la classe, mientras que la segunda 'x' se refiere al parametro de la funcion 'isToTheRight'
         return self.x > x
     }
+    
+    /*
+     En Swift, las estructuras (struct) y enumeraciones (enum) son tipos de valor, lo que significa que sus instancias se copian cuando se asignan o se pasan a funciones. Por defecto, sus métodos no pueden modificar sus propias propiedades. Al marcar un método con la palabra clave mutating, le indicas al compilador que ese método va a modificar (o "mutar") la instancia, permitiéndote cambiar sus propiedades internamente.
+     */
+    mutating func moveBy(x deltaX: Double, y deltaY: Double) {
+        //self.x += deltaY
+        //self.y += deltaY
+        Point(x: self.x + deltaX, y:self.y + deltaY)
+    }
 }
 
-let somePoint = Point(x: 4, y: 5)
+var somePoint = Point(x: 4, y: 5)
+//let somePoint = Point(x: 4, y: 5)
 somePoint.isToTheRight(of: 1)
 somePoint.isToTheRight(of: 54)
+
+somePoint.moveBy(x: 2, y: -3)
+somePoint.x = 9
+
+enum DifferentStateSwitch {
+    case off, low, high
+    mutating func next() {
+        switch self {
+            case .off:
+                self = .low
+            case .low:
+                self = .high
+            case .high:
+                self = .off
+        }
+    }
+}
+
+var controllerStatis = DifferentStateSwitch.off
+controllerStatis.next()
+controllerStatis.next()
+controllerStatis.next()
+
+
