997b3a3b44eee05c1c9162a3f4d47f5eaa3a36e7
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-02-22 15:35:30 +0100

    Optional chaining: example

--- OptionalChaining.playground/Contents.swift
+++ OptionalChaining.playground/Contents.swift
@@ -1,3 +1,4 @@
+/*
 class Person {
     var residence: Residence?
 }
@@ -19,4 +20,46 @@ if let roomCount = edgar.residence?.numberOfRooms {
     print("La casa de edgar tiene \(roomCount) habitaciones")
 } else {
     print("Edgar no tiene casa")
+}*/
+
+class Person {
+    var residence: Residence?
+}
+
+class Residence {
+    var rooms = [Room]()
+    var numberOfRooms: Int {
+        return rooms.count
+    }
+    subscript(i: Int) ->Room{
+        get {
+            return rooms[i]
+        }
+        set {
+            rooms[i] = newValue
+        }
+    }
+}
+
+class Room{
+    let name: String
+    init(name: String) {
+        self.name = name
+    }
+}
+
+class Address {
+    var buildingName: String?
+    var buildingNumber: String?
+    var street: String?
+    
+    func buildingIdentifier() -> String? {
+        if let buildingNumber = buildingNumber, let street = street {
+            return "\(buildingNumber), \(street)"
+        } else if let buildingName = buildingName {
+            return buildingName
+        } else {
+            return nil
+        }
+    }
 }
